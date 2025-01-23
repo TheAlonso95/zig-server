cdfafbab73a4538ac7b5f4d47dd078eca36fdf92
Rafael Alonso Medeiros <rafael.a.m95@hotmail.com>
2025-01-22 23:50:59 -0100

    feat: init server

--- src/main.zig
+++ src/main.zig
@@ -1,45 +1,21 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const global = struct {
-        fn testOne(input: []const u8) anyerror!void {
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(global.testOne, .{});
-}
-
 const std = @import("std");
 
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("zig-server_lib");
+pub fn main() !void {
+    const address = try std.net.Address.parseIp4("127.0.0.1", 8080);
+
+    var server = try address.listen(std.net.Address.ListenOptions{});
+    defer server.deinit();
+
+    while (true) {
+        try handleConnection(try server.accept());
+    }
+}
+
+// Simple test will change later
+fn handleConnection(conn: std.net.Server.Connection) !void {
+    defer conn.stream.close();
+    var buffer: [1024]u8 = undefined;
+    var http_server = std.http.Server.init(conn, &buffer);
+    var req = try http_server.receiveHead();
+    try req.respond("hello world\n", std.http.Server.Request.RespondOptions{});
+}
